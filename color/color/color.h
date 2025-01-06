6e9b36ae234240a4e5332149d9864ac1bc22ae29
watan104 <96546018+watan104@users.noreply.github.com>
2025-01-07 01:40:58 +0300

    Fix
    Co-Authored-By: Nedret <153279327+aeolus1001@users.noreply.github.com>

--- color/color/color.h
+++ color/color/color.h
@@ -5,17 +5,32 @@
 
 #ifndef __COLOR_H__
 #include <fstream>
+#include <string>
+#if COMPILER_VER >= 202002L
 #include <format.h>
+#endif
 
 class color {
 public:
 	color(int r, int g, int b) : r_(r), g_(g), b_(b) {}
 	friend std::ostream& operator<<(std::ostream& os, const color& color) {
+#if COMPILER_VER >= 202002L
 		return os << std::format("\x1B[38;2;{};{};{}m", color.r_, color.g_, color.b_);
+#else
+		std::stringstream fmt;
+		fmt << "\x1B[38;2;" << color.r_ << ";" << color.g_ << ";" << color.b_ <<"m";
+		return os << fmt.str();
+#endif
 	}
 	~color() {}
 	friend std::ostream& operator<<(std::ostream& os) {
-		return os << std::::format("\x1B[38;2;255;255;255m");
+#if COMPILER_VER >= 202002L
+		return os << std::format("\x1B[38;2;255;255;255m");
+#else
+		std::stringstream fmt;
+		fmt << "\x1B[38;2;255;255;255m";
+		return os << fmt.str();
+#endif
 	}
 private:
 	int r_, g_, b_;
